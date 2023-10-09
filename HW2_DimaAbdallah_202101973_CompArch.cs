b686d8268aeadfac3676022b223533b983da68cd
dmabdallah <147241009+dmabdallah@users.noreply.github.com>
2023-10-09 19:55:58 +0300

    Create HW2_DimaAbdallah_202101973_CompArch.cs

---
+++ HW2_DimaAbdallah_202101973_CompArch.cs
@@ -0,0 +1,41 @@
+using Systems;
+using System.IO.Ports;
+using System.Threading;
+
+class Program{
+    static void Main()
+    {
+        using(SerialPort serialPort=new SerialPort("COM4",9600))
+        {
+            try
+            {
+                serialPort.Open();
+                Console.WriteLine("The required cerial port is opened and is ready for communication.")
+
+                Thread.Sleep(2000);
+
+                while(true)
+                {
+                    serialPort.Write("x");
+                    Console.WriteLine("Sent:x");
+                    Thread.Sleep(2000);
+
+                }
+                catch(Exceptioin ex)
+                {
+                    Console.WriteLine("Error: "+ ex.Message);
+
+                }
+                finally
+                {
+                    if(serialPort.IsOpen)
+                    {
+                        serialPort.Cole();
+                        Console.WriteLine("The serial port is now closed, and serial communication is ended.")
+
+                    }
+                }
+            }
+        }
+    }
+}
